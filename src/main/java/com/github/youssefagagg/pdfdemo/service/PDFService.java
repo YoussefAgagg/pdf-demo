d3239db2dfc73669fba5b4c20b1329facad67a58
Youssef Agagg <YoussefAgagg@gmail.com>
2023-05-13 21:09:07 +0300

    refactoring

--- src/main/java/com/github/youssefagagg/pdfdemo/service/PDFService.java
+++ src/main/java/com/github/youssefagagg/pdfdemo/service/PDFService.java
@@ -2,9 +2,7 @@ package com.github.youssefagagg.pdfdemo.service;
 
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface PDFService {
     ResponseEntity<Resource> generateUsersReportPDF() throws Exception;
 }
