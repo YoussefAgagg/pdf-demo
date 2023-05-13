4d04bff4c6646d7810c84e6313c2512c97acdb6b
Youssef Agagg <YoussefAgagg@gmail.com>
2023-05-13 21:07:52 +0300

    refactoring

--- src/main/java/com/github/youssefagagg/pdfdemo/controller/PDFController.java
+++ src/main/java/com/github/youssefagagg/pdfdemo/controller/PDFController.java
@@ -1,24 +1,18 @@
 package com.github.youssefagagg.pdfdemo.controller;
 
 import com.github.youssefagagg.pdfdemo.service.PDFService;
-import com.lowagie.text.DocumentException;
 import lombok.RequiredArgsConstructor;
-import net.sf.jasperreports.engine.JRException;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
 public class PDFController {
     private final PDFService pdfService;
 
-    @GetMapping("/html/users-pdf-generator")
+    @GetMapping("/users/report")
     public ResponseEntity<Resource> generateUsersReportPDFHtml() throws Exception {
         return pdfService.generateUsersReportPDF();
 
